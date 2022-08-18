3de03fba0e070eb40afe5301d2c2c49268e19af5
mengjun <314008304@qq.com>
2022-08-18 22:23:09 +0800

    'inittemplate'

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+console.log(process.env.NODE_ENV, process.env.APP_ENV, '12311233')
 module.exports = defineConfig({
   transpileDependencies: true
 })
